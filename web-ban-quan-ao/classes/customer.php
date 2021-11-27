95dd7bc361b04c764ab6507c75cda03c8f119d76
WebNgoc <buingoc1562000@gmail.com>
2021-11-27 21:35:04 +0700

    new

--- web-ban-quan-ao/classes/customer.php
+++ web-ban-quan-ao/classes/customer.php
@@ -34,30 +34,30 @@
 				$check_email = "SELECT * FROM dangky WHERE email='$email' LIMIT 1";
 				$result_check = $this->db->select($check_email);
 				if($result_check){
-					$alert = "<span style ='color:red; font-size: 20px;'>Email Already Existed ! Please Enter Another Email</span>";
+					$alert = "<span style ='color:red; font-size: 20px;'>Email này đã đăng ký</span>";
 					return $alert;
 				}else{
 					$query = "INSERT INTO dangky(tenkhachhang,email,diachinhan,dienthoai,matkhau) 
                     VALUES('$tenkhachhang','$email','$diachinhan','$dienthoai','$matkhau')";
 					$result = $this->db->insert($query);
 					if($result){
-						$alert = "<span style ='color:green; font-size: 20px;'>Customer Created Successfully</span>";
+						$alert = "<span style ='color:green; font-size: 20px;'>Đăng ký thành công</span>";
 						return $alert;
 					}else{
-						$alert = "<span style ='color:red; font-size: 20px;'>Customer Created Not Successfully</span>";
+						$alert = "<span style ='color:red; font-size: 20px;'>Dăng ký không thành côn</span>";
 						return $alert;
 					}
 				}
 			}
 		}
 		public function login_customers($data){
-			$email = mysqli_real_escape_string($this->db->link, $data['email']);
+			$tenkhachhang = mysqli_real_escape_string($this->db->link, $data['tenkhachhang']);
 			$matkhau = mysqli_real_escape_string($this->db->link, md5($data['matkhau']));
-			if($email=="" || $matkhau==""){
+			if($tenkhachhang=="" || $matkhau==""){
 				$alert = "<span style ='color:red; font-size: 15px;'>Không được để trống email vs mật khẩu</span>";
 				return $alert;
 			}else{
-				$check_login = "SELECT * FROM dangky WHERE email='$email' AND matkhau='$matkhau'";
+				$check_login = "SELECT * FROM dangky WHERE tenkhachhang='$tenkhachhang' AND matkhau='$matkhau'";
 				$result_check = $this->db->select($check_login);
 				if($result_check){
 
@@ -65,6 +65,7 @@
 					Session::set('customer_login',true);
 					Session::set('customer_id',$value['id_khachhang']);
 					Session::set('customer_name',$value['tenkhachhang']);
+                    // header('location:index.php');
                     $alert = "<span style ='color:green; font-size: 20px;'>Đăng nhập thành công <a href='index.php'>Đến trang chủ</a></span>";
                     return $alert;
 				
--- web-ban-quan-ao/login.php
+++ web-ban-quan-ao/login.php
@@ -37,7 +37,7 @@
             
             <div class="form-group">
                 <i class="fas fa-user"></i>
-                <input type="text" name="email" class="form-input" placeholder="email">
+                <input type="text" name="tenkhachhang" class="form-input" placeholder="email">
             </div>
             <div class="form-group">
                 <i class="fas fa-key"></i>
