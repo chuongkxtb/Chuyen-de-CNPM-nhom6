0a8ed8b0a4dda76caaa695a5b9cb1632d6d5933a
WebNgoc <buingoc1562000@gmail.com>
2021-11-27 19:51:12 +0700

    new

--- web-ban-quan-ao/search.php
+++ web-ban-quan-ao/search.php
@@ -30,11 +30,11 @@
 	      	 	while($result = $search_product->fetch_assoc()){
 	      	?>
 				<div class="grid_1_of_4 images_1_of_4">
-					 <a href="preview-3.php"><img src="admin/uploads/<?php echo $result['hinhanh'] ?>" width="200px" alt="" /></a>
-					 <h2><?php echo $result['tensp'] ?></h2>
-					 <p><?php echo $fm->textShorten($result['noidung'],50); ?></p>
-					 <p><span class="price"><?php echo $result['giadexuat']." "."VNĐ" ?></span></p>
-				     <div class="button"><span><a href="details.php?proid=<?php echo $result['idsanpham'] ?>" class="details">Details</a></span></div>
+					 <a href="details.php"><img src="images/<?php echo $result['hinhanh']?>" alt="" /></a>
+					 <h2><?php echo $result['tensp']?> </h2>
+					 <p><?php echo $fm->textShorten($result['noidung'],20)?></p>
+					 <p><span class="price"><?php echo $result['giadexuat']?></span></p>
+				     <div class="button"><span><a href="details.php?idsp=<?php echo $result['idsanpham']?>" class="details">Details</a></span></div>
 				</div>
 			<?php
 			}
